e53975db3412c85bab42845cb55c883c1017c231
SUSANTxBOTS <susantpc1232@gmail.com>
2025-10-07 19:12:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 7968389767))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Susantxbotz_support")
+OWNER_ID = int(getenv("OWNER_ID", 8156708830))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Thronex_Chats")
